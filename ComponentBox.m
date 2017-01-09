b52b1f7ba3a619c9c71ac61c13c1a6e1cff7803e
VIVEK ROY <VIVEK ROY>
2017-01-09 17:30:17 +0530

    BoundingBox complted

--- ComponentBox.m
+++ ComponentBox.m
@@ -1,6 +1,18 @@
-function [ output_args ] = ComponentBox( img )
+function ComponentBox(file)
 %COMPONENTBOX Summary of this function goes here
 %   Detailed explanation goes here
-    
+    img = imread(file);
+    glevel = graythresh(img);
+    bwimg = im2bw(img, glevel);
+    bwimg = ~bwimg;
+    CC = bwconncomp(bwimg, 8);
+    stats = regionprops(CC, 'BoundingBox');
+    hold on;
+    imshow(img);
+    for k = 1 : length(stats)
+        thisBB = stats(k).BoundingBox;
+        rectangle('Position', thisBB, 'EdgeColor', 'r');
+    end
+    hold off;
 end
 
